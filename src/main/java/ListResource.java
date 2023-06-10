bf8134b236d7f077241c4803727a85c7b397fbb2
Eugene Popov <eugene.popov@list.ru>
2023-06-10 22:36:00 +0300

    lesson 13-90

---
+++ src/main/java/ListResource.java
@@ -0,0 +1,72 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/*
+    Request from https://reqres.in/api/unknown
+*/
+
+public class Resource {
+
+    private int page;
+
+    @JsonProperty("per_page")
+    private int perPage;
+
+    private int total;
+
+    @JsonProperty("total_pages")
+    private int totalPages;
+
+    private List<User> data;
+
+    private ResorceSupport support;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+
+    public ResorceSupport getSupport() {
+        return support;
+    }
+
+    public void setSupport(ResorceSupport support) {
+        this.support = support;
+    }
+}
